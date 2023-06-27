1221ec7e6e576494f968309b6c42d55f463a94e6
miklik1 <martin.miklik@brandmaster.com>
2023-06-27 16:48:55 +0200

    banner progress

--- components/banner.js
+++ components/banner.js
@@ -1,9 +1,16 @@
-const Banner = () => {
+import styles from "./banner.module.css";
+
+const Banner = (props) => {
+
+  const handleOnBannerBtnClick = (e) => {
+    console.log(e.target);
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Coffee Connoisseur</h1>
-      <p>Discover your local coffee shops!</p>
-      <button>View stores nearby</button>
+      <h1 className={styles.title}>Coffee Connoisseur</h1>
+      <p className={styles.subtitle}> — Discover your local coffee shops!</p>
+      <button className={styles.button} onClick={handleOnBannerBtnClick}>{props.buttonText}</button>
     </div>
   );
 };
