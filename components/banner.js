7261d0cb17c04603b5e9d07a0c52aff05e134fde
miklik1 <martin.miklik@brandmaster.com>
2023-06-27 15:02:43 +0200

    first commit

---
+++ components/banner.js
@@ -0,0 +1,11 @@
+const Banner = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Coffee Connoisseur</h1>
+      <p>Discover your local coffee shops!</p>
+      <button>View stores nearby</button>
+    </div>
+  );
+};
+
+export default Banner;
