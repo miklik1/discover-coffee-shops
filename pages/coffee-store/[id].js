03bf008872e2f68bef42b9eb53da3edbcb458767
Martin Miklik <martin.miklik@brandmaster.com>
2023-07-10 20:57:45 +0200

    progress

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -4,22 +4,23 @@ import Link from "next/link";
 import Image from "next/image";
 import cls from "classnames";
 
-import coffeeStoresData from "../../data/coffee-stores.json";
-
 import styles from "../../styles/coffee-store.module.css";
+import { fetchCoffeeStores } from "@/lib/coffee-stores";
 
 export async function getStaticProps({ params }) {
+  const coffeeStores = await fetchCoffeeStores();
   return {
     props: {
-      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+      coffeeStore: coffeeStores.find((coffeeStore) => {
         return coffeeStore.id.toString() === params.id;
       }),
     },
   };
 }
 
-export function getStaticPaths() {
-  const paths = coffeeStoresData.map((coffeeStore) => {
+export async function getStaticPaths() {
+  const coffeeStores = await fetchCoffeeStores();
+  const paths = coffeeStores.map((coffeeStore) => {
     return {
       params: {
         id: coffeeStore.id.toString(),
@@ -39,11 +40,11 @@ const CoffeeStore = ({ coffeeStore }) => {
     return <div>Loading...</div>;
   }
 
-  const { name, address, neighbourhood, imgUrl } = coffeeStore;
+  const { name, neighborhood, address, imgUrl } = coffeeStore;
 
   const handleUpvoteButton = () => {
-    console.log('handle upvote');
-  }
+    console.log("handle upvote");
+  };
 
   return (
     <div className={styles.layout}>
@@ -53,13 +54,16 @@ const CoffeeStore = ({ coffeeStore }) => {
       <div className={styles.container}>
         <div className={styles.col1}>
           <div className={styles.backToHomeLink}>
-            <Link href="/">Back</Link>
+            <Link href="/">← Back</Link>
           </div>
           <div className={styles.nameWrapper}>
             <h1 className={styles.name}>{name}</h1>
           </div>
           <Image
-            src={imgUrl}
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
             width={600}
             height={360}
             className={styles.storeImg}
@@ -67,19 +71,40 @@ const CoffeeStore = ({ coffeeStore }) => {
           />
         </div>
         <div className={cls("glass", styles.col2)}>
+          {address && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src="/static/icons/places.svg"
+                width="24"
+                height="24"
+                alt="places-icon"
+              />
+              <p className={styles.text}>{address}</p>
+            </div>
+          )}
+          {address && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src="/static/icons/nearMe.svg"
+                width="24"
+                height="24"
+                alt="near me icon"
+              />
+              <p className={styles.text}>{neighborhood}</p>
+            </div>
+          )}
           <div className={styles.iconWrapper}>
-            <Image src="/static/icons/places.svg" width="24" height="24" alt="places-icon"/>
-          <p className={styles.text}>{address}</p>
+            <Image
+              src="/static/icons/star.svg"
+              width="24"
+              height="24"
+              alt="star icon"
+            />
+            <p className={styles.text}>1</p>
           </div>
-          <div className={styles.iconWrapper}>
-            <Image src="/static/icons/nearMe.svg" width="24" height="24" alt="near me icon"/>
-          <p className={styles.text}>{neighbourhood}</p>
-          </div>
-          <div className={styles.iconWrapper}>
-            <Image src="/static/icons/star.svg" width="24" height="24" alt="star icon"/>
-          <p className={styles.text}>1</p>
-          </div>
-          <button className={styles.upvoteButton} onClick={handleUpvoteButton}> Up vote!</button>
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,13 @@ import coffeeIcon from "../public/static/day79-coffee.svg";
 import Banner from "@/components/banner";
 import Card from "@/components/card";
 
-import coffeeStoresData from "../data/coffee-stores.json";
+import { fetchCoffeeStores } from "@/lib/coffee-stores";
 
 export async function getStaticProps(context) {
+  const coffeeStores = await fetchCoffeeStores();
+
   return {
-    props: { coffeeStores: coffeeStoresData },
+    props: { coffeeStores },
   };
 }
 
@@ -29,14 +31,17 @@ export default function Home({ coffeeStores }) {
         </div>
         {coffeeStores.length > 0 && (
           <>
-            <h2 className={styles.heading2}>Toronto stores</h2>
+            <h2 className={styles.heading2}>Zlín stores</h2>
             <div className={styles.cardLayout}>
               {coffeeStores.map((store) => {
                 return (
                   <Card
                     key={store.id}
                     name={store.name}
-                    imgUrl={store.imgUrl}
+                    imgUrl={
+                      store.imgUrl ||
+                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                    }
                     href={`/coffee-store/${store.id}`}
                     className={styles.card}
                   />
